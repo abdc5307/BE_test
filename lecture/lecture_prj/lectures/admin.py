809c11f9a56b5a01af20fba7d739f9c926d6c05e
unknown <abdc5307@gmail.com>
2026-05-07 11:06:08 +0900

    [0507] 이정은

--- lecture/lecture_prj/lectures/admin.py
+++ lecture/lecture_prj/lectures/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from lectures.models import Professor, Lecture, Student
+from lectures.models import Professor, Lecture, Student,LectureStudent
 
 admin.site.register(Professor)
 admin.site.register(Lecture)
 admin.site.register(Student)
+admin.site.register(LectureStudent)
--- lecture/lecture_prj/lectures/models.py
+++ lecture/lecture_prj/lectures/models.py
@@ -11,15 +11,24 @@ class Lecture(models.Model):
     title = models.CharField(max_length = 50)
     code = models.CharField(max_length = 20, unique= True)
     professor = models.ForeignKey(to= Professor, on_delete = models.CASCADE, related_name ='lectures')
+    
+
     def __str__(self):
         return f'{self.title}'
     
 class Student(models.Model):
     name = models.CharField(max_length = 20)
     student_id = models.CharField(max_length=20, unique= True)
-    lecture = models.ForeignKey(to = Lecture, on_delete = models.CASCADE, null = True, related_name='students' )
+    lecture = models.ManyToManyField(to = Lecture, through = "LectureStudent", related_name='students' )
 
     def __str__(self):
         return f'[{self.student_id}] {self.name}'
     
+class LectureStudent(models.Model):
+    lecture = models.ForeignKey(to = Lecture, on_delete = models.CASCADE, related_name = "lecture_students")
+    student = models.ForeignKey(to = Student, on_delete = models.CASCADE, related_name = "lecture_students")
+    enrolled_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f'[{ self.lecture.title}] { self.student.name}' 
 
